8311bfd116789ef1514838f546b901e2f839bb15
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-11-22 23:00:15 +0100

    introduce network service
    [#107994250]

---
+++ Roomguru/Source Files/Networking/NetworkService.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkService.swift
+//  Roomguru
+//
+//  Created by Radoslaw Szeja on 21/11/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkService {
+    var baseURL: NSURL { get }
+    var scheme: String { get }
+    var serviceType: NSURLRequestNetworkServiceType { get }
+}
