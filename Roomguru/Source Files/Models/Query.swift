f1c0525e967d9e8c17b924db62310428178dc77a
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-11-22 23:38:35 +0100

    introduce simpler query in place of queryable
    [#107994250]

---
+++ Roomguru/Source Files/Models/Query.swift
@@ -0,0 +1,27 @@
+//
+//  Query.swift
+//  Roomguru
+//
+//  Created by Radoslaw Szeja on 23/03/15.
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case GET = "GET"
+    case POST = "POST"
+    case PUT = "PUT"
+    case DELETE = "DELETE"
+}
+
+protocol Query {
+    var method: Method { get }
+    var path: String { get }
+    var parameters: Parameters? { get }
+    var service: SecureNetworkService { get }
+}
+
+protocol GoogleRequiredAuthProtocol {
+    func setFullPath(baseUrl: String, authKey: String)
+}
