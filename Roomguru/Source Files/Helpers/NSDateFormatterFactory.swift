4e8a4b55e37e9e84aca08e0b575639c7ddb68c29
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-11-23 18:20:30 +0100

    add google date formatter factory method
    [#107994250]

---
+++ Roomguru/Source Files/Helpers/NSDateFormatterFactory.swift
@@ -0,0 +1,18 @@
+//
+//  NSDateFormatterFactory.swift
+//  Roomguru
+//
+//  Created by Radoslaw Szeja on 23/11/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+extension NSDateFormatter {
+    class func googleDateFormatter() -> NSDateFormatter {
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        formatter.timeZone = NSTimeZone.localTimeZone()
+        return formatter
+    }
+}
