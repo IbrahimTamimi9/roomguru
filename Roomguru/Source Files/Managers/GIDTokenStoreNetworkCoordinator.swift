bbc9db26b2a6402238e41066a8a5392e7858bd4a
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-11-25 14:55:39 +0100

    use google sign in token refresh mechanism
    [#107994250]

--- Roomguru/Source Files/Managers/GIDTokenStoreNetworkCoordinator.swift
+++ Roomguru/Source Files/Managers/GIDTokenStoreNetworkCoordinator.swift
@@ -13,29 +13,18 @@ class GIDTokenStoreNetworkCoordinator {
     
     func refreshAccessToken(parameters parameters: [String: AnyObject], completion: ((tokenInfo: (accessToken: String, expirationDate: NSDate)?, error: NSError?)-> Void)) {
         
-        Alamofire
-            .request(.POST, Constants.GooglePlus.RefreshTokenURL, parameters: parameters)
-            .responseJSON { response in
-                
-                if let error = response.result.error {
+        let error = NSError(message: NSLocalizedString("Session expired. Please log in again.", comment: ""))
+        completion(tokenInfo: nil, error: error)
+        
+        if let currentUser = GIDSignIn.sharedInstance().currentUser {
+            currentUser.authentication.refreshTokensWithHandler { (auth, error) -> Void in
+                if let error = error {
                     completion(tokenInfo: nil, error: error)
-                    return
-                    
-                } else if let data: AnyObject = response.result.value {
-                    
-                    let json = JSON(data)
-                    
-                    if let accessToken = json["access_token"].string, expiresIn = json["expires_in"].int {
-                        
-                        let timeInterval = NSTimeInterval(expiresIn)
-                        let tokenInfo = (accessToken: accessToken, expirationDate: NSDate().dateByAddingTimeInterval(timeInterval))
-                        completion(tokenInfo: tokenInfo, error: response.result.error)
-                        return
-                    }
+                } else if let accessToken = auth.accessToken, expirationDate = auth.accessTokenExpirationDate {
+                    let tokenInfo = (accessToken: accessToken, expirationDate: expirationDate)
+                    completion(tokenInfo: tokenInfo, error: nil)
                 }
-                
-                let error = NSError(message: NSLocalizedString("Session expired. Please log in again.", comment: ""))
-                completion(tokenInfo: nil, error: error)
+            }
         }
     }
 }
